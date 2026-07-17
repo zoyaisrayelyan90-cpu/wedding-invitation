ca0c8d570dffc0f46d4b5e96fbfb58742fde770f
zoyaisrayelyan90-cpu <zoyaisrayelyan90@gmail.com>
2026-07-18 01:50:44 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Տեղադրեք Google Apps Script Web app-ի /exec հղումը չակերտների մեջ։
-window.RSVP_ENDPOINT = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_EXEC_URL_HERE";
+window.RSVP_ENDPOINT = "https://script.google.com/macros/s/AKfycbwnk5x9KS20oHQPRs7KAgxbLsh8ASdNmxs3GNoCSrPum0nHNNJ_an47AKc8ZxHEgZtL/exec";
